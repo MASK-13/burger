0be95a5ae7d825311296c6564708e59e7f40284f
Mike Morales <mikemorale@live.com>
2018-02-21 00:57:29 -0700

    broken

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-seeds.sql
+INSERT INTO burger (burger_name) VALUES ("red,white and blue");
+INSERT INTO burger (burger_name) VALUES	("Swiss and Avocado");
+INSERT INTO burger (burger_name) VALUES ("Tasty and Juicy");
